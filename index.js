d094bb09a53a843cd711a21bbf2e5c3410d9484b
gilbert <gilbert.bageni@student.moringaschool.com>
2023-07-04 22:38:23 +0300

    completed lab

--- index.js
+++ index.js
@@ -5,6 +5,30 @@ function hasTargetSum(array, target) {
 /* 
   Write the Big O time complexity of your function here
 */
+// index.js
+
+function hasTargetSum(arr, target) {
+  const numSet = new Set();
+
+  for (let i = 0; i < arr.length; i++) {
+    const complement = target - arr[i];
+    if (numSet.has(complement)) {
+      return true;
+    }
+    numSet.add(arr[i]);
+  }
+
+  return false;
+}
+
+// Test cases
+console.log(hasTargetSum([3, 8, 12, 4, 11, 7], 10)); // true
+console.log(hasTargetSum([22, 19, 4, 6, 30], 25)); // true
+console.log(hasTargetSum([1, 2, 5], 4)); // false
+console.log(hasTargetSum([-7, 10, 4, 8], 3)); // true
+console.log(hasTargetSum([1, 2, 3, 4], 5)); // true
+console.log(hasTargetSum([2, 2, 3, 3], 4)); // true
+console.log(hasTargetSum([4], 4)); // false
 
 /* 
   Add your pseudocode here
